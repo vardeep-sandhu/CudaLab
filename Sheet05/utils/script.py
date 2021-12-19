d8d03b2da11b2818780447f40e33be5db08e692c
here-to-learn0 <good.coding.is.that.you@gmail.com>
2021-12-19 17:27:47 +0100

    added torch LSTM

--- Sheet05/utils/script.py
+++ Sheet05/utils/script.py
@@ -1,5 +1,121 @@
+import os
+from tqdm import tqdm
 import numpy as np
-import torch
+import matplotlib.pyplot as plt
 
-def print_some(*args):
-    print(args)
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import datasets, models, transforms
+
+def train_epoch(model, train_loader, optimizer, criterion, epoch, device):
+    """ Training a model for one epoch """
+    
+    loss_list = []
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader))
+    for i, (images, labels) in progress_bar:
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        # Clear gradients w.r.t. parameters
+        optimizer.zero_grad()
+         
+        # Forward pass to get output/logits
+        outputs = model(images)
+         
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = criterion(outputs, labels)
+        loss_list.append(loss.item())
+         
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+         
+        # Updating parameters
+        optimizer.step()
+        
+        progress_bar.set_description(f"Epoch {epoch+1} Iter {i+1}: loss {loss.item():.5f}. ")
+        
+    mean_loss = np.mean(loss_list)
+    return mean_loss, loss_list
+
+
+@torch.no_grad()
+def eval_model(model, eval_loader, criterion, device):
+    """ Evaluating the model for either validation or test """
+    correct = 0
+    total = 0
+    loss_list = []
+    
+    for images, labels in eval_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        # Forward pass only to get logits/output
+        outputs = model(images)
+                 
+        loss = criterion(outputs, labels)
+        loss_list.append(loss.item())
+            
+        # Get predictions from the maximum value
+        preds = torch.argmax(outputs, dim=1)
+        labels = torch.argmax(labels, dim=1)
+        correct += len(torch.where(preds==labels)[0])
+        total += len(labels)
+    # Total correct predictions and loss
+    accuracy = correct / total * 100
+    loss = np.mean(loss_list)
+    
+    return accuracy, loss
+
+
+def train_model(model, optimizer, scheduler, criterion, train_loader, valid_loader, num_epochs, device):
+    """ Training a model for a given number of epochs"""
+    
+    train_loss = []
+    val_loss =  []
+    loss_iters = []
+    valid_acc = []
+    
+    for epoch in range(num_epochs):
+           
+        # validation epoch
+        model.eval()  # important for dropout and batch norms
+        accuracy, loss = eval_model(
+                    model=model, eval_loader=valid_loader,
+                    criterion=criterion, device=device
+            )
+        valid_acc.append(accuracy)
+        val_loss.append(loss)
+        
+        # training epoch
+        model.train()  # important for dropout and batch norms
+        mean_loss, cur_loss_iters = train_epoch(
+                model=model, train_loader=train_loader, optimizer=optimizer,
+                criterion=criterion, epoch=epoch, device=device
+            )
+        scheduler.step()
+        train_loss.append(mean_loss)
+        loss_iters = loss_iters + cur_loss_iters
+        
+        print(f"Epoch {epoch+1}/{num_epochs}")
+        print(f"    Train loss: {round(mean_loss, 5)}")
+        print(f"    Valid loss: {round(loss, 5)}")
+        print(f"    Accuracy: {accuracy}%")
+        print("\n")
+    
+    print(f"Training completed")
+    return train_loss, val_loss, loss_iters, valid_acc
+
+
+def smooth(f, K=5):
+    """ Smoothing a function using a low-pass filter (mean) of size K """
+    kernel = np.ones(K) / K
+    f = np.concatenate([f[:int(K//2)], f, f[int(-K//2):]])  # to account for boundaries
+    smooth_f = np.convolve(f, kernel, mode="same")
+    smooth_f = smooth_f[K//2: -K//2]  # removing boundary-fixes
+    return smooth_f
+
+def count_model_params(model):
+    """ Counting the number of learnable parameters in a nn.Module """
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return num_params
